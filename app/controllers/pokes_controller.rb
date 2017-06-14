a80662c8b6c9ea95052b9e6005decf7b73d5d5a2
Protium1 <protium1@outlook.com>
2017-06-14 13:40:53 -0500

    prueba de listado

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -4,7 +4,7 @@ class PokesController < ApplicationController
   # GET /pokes
   # GET /pokes.json
   def index
-    @pokes = Poke.select("number").order("number DESC")
+    @pokes = Poke.select("number, name, picture").where(:acquired => false).order("number DESC")
   end
 
   # GET /pokes/1
